e24655c046cff3e20bcf7898d0ed5f08e108849f
JulioCesarBarbosaCastroGithub <juliocesarbarbosacastro@outlook.com>
2024-07-09 09:10:03 -0300

    second commit

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ let alunoNotaVidaECarreira = 9;
 
 let alunoMediaNotas = (alunoNotaUsabilidade + alunoNotaEngenhariaDeSoftware + alunoNotaVidaECarreira) / 3;
 
+console.log("Olá, a sua média é: " + alunoMediaNotas.toFixed(2));
+
 if(alunoMediaNotas > 0 && alunoMediaNotas < 5){
     console.log("Você está reprovado, estude e tente novamente.");
 }else if(alunoMediaNotas > 4 && alunoMediaNotas < 8){
