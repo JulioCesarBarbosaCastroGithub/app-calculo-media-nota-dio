d32883d992b5607c7852eae6a83779b8a68133c3
JulioCesarBarbosaCastroGithub <juliocesarbarbosacastro@outlook.com>
2024-07-09 09:02:10 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let alunoNotaUsabilidade = 1;
+let alunoNotaEngenhariaDeSoftware = 1;
+let alunoNotaVidaECarreira = 9;
+
+let alunoMediaNotas = (alunoNotaUsabilidade + alunoNotaEngenhariaDeSoftware + alunoNotaVidaECarreira) / 3;
+
+if(alunoMediaNotas > 0 && alunoMediaNotas < 5){
+    console.log("Você está reprovado, estude e tente novamente.");
+}else if(alunoMediaNotas > 4 && alunoMediaNotas < 8){
+    console.log("Você está de recuperação, estude e tente novamente.");
+}else{
+    console.log("Você está aprovado, parabéns!");
+};
